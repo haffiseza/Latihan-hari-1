481362cba80aadcdca878ca5e1640beb6cd21b23
haffis eza <64406752+haffiseza@users.noreply.github.com>
2021-01-19 21:10:48 +0700

    Add files via upload

---
+++ latihan_hari_1.py
@@ -0,0 +1,184 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+print(10000000000000000000000000000000000000000000000000000000000000000000001 + 1)
+
+
+# In[8]:
+
+
+print(10)
+print(type(10))
+
+
+# In[10]:
+
+
+print(4.2)
+print(type(4.2))
+
+
+# In[11]:
+
+
+print("Hacktiv8")
+print(type("Hacktiv8"))
+
+
+# In[12]:
+
+
+print('hacktiv8')
+
+
+# In[13]:
+
+
+print(type('Hactive8'))
+
+
+# In[15]:
+
+
+print(type(True))
+
+
+# In[17]:
+
+
+print(type(False))
+
+
+# In[18]:
+
+
+a = 300
+b = 200
+c = a + b
+print(a, b, c)
+
+
+# In[20]:
+
+
+name = "Hacktiv8"
+Age = 40
+has_laptops = True 
+print(name, Age, has_laptops)
+
+
+# In[24]:
+
+
+a = 2
+b = 3
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+print(a ** b)
+
+
+# In[26]:
+
+
+# + operators
+s = 'foo'
+t = 'bar'
+u = 'baz'
+
+print(s + t)
+
+print(s + t + u)
+
+
+print('Hacktiv8' + 'PTP')
+
+
+# In[28]:
+
+
+tas_belanja = ['aqua','pringles','silverqueen']
+
+
+# In[29]:
+
+
+print(tas_belanja)
+
+
+# In[36]:
+
+
+tas_belanja = ['aqua','pringles','silverqueen','bembeng','kopi good day','pocarisweat']
+print(tas_belanja[0])
+
+
+# In[37]:
+
+
+print(tas_belanja[3])
+
+
+# In[39]:
+
+
+print(tas_belanja[-1])
+
+
+# In[43]:
+
+
+tas_belanja[3:6]
+
+
+# In[46]:
+
+
+del tas_belanja[3]
+print(tas_belanja)
+
+
+# In[47]:
+
+
+tas_belanja = ['aqua','pringles','silverqueen','bembeng','kopi good day','pocarisweat']
+print(tas_belanja[0])
+del tas_belanja[3]
+print(tas_belanja)
+
+
+# In[48]:
+
+
+tas_belanja = {'aqua':'mas haffis',
+               'pringles':'bak ria',
+               'silverqueen':'mas didi',
+               'bembeng':'bak ria',
+               'kopi good day':'bak nita',
+               'tisu paseo':'bak karamina'}
+print(tas_belanja)
+
+
+# In[49]:
+
+
+print(tas_belanja['aqua'])
+
+
+# In[51]:
+
+
+print(tas_belanja['pringles'])
+
+
+# In[ ]:
+
+
+
+
